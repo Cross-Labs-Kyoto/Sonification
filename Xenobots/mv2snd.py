5b5cfc2dbb168d4682f8c4b7aef765f4b92621bf
davinellulinvega <casparluc@gmail.com>
2025-04-05 21:22:03 +0900

    Re-implement the movement to sound mapping as a learning process.

--- Xenobots/mv2snd.py
+++ Xenobots/mv2snd.py
@@ -2,181 +2,170 @@
 from argparse import ArgumentParser
 from pathlib import Path
 import numpy as np
-from scipy.io import wavfile
-import cv2 as cv
+import torch
+import torch.nn.functional as F
 import librosa
-from tqdm import tqdm
+from pedalboard.io import AudioFile, AudioStream
+from loguru import logger
 
-from utils import MvTracker, cart_to_polar, get_video_meta
-from settings import SAMPLE_RATE
+from utils import MvTracker, get_video_meta, VideoIterator
+from settings import ROOT_DIR
 
 
-parser = ArgumentParser()
-parser.add_argument('filename', type=str)
-parser.add_argument('-t', '--thres', dest='canny_thres', type=int, default=40)
+class Speed2SoundModel(object):
+    """Define a mapping taking rotation speed as input and translate it to frequency."""
 
-args = parser.parse_args()
-vid_file = Path(args.filename).expanduser().resolve()
-if not vid_file.exists() or not vid_file.is_file():
-    exit(-1)
+    def __init__(self, f_min=20, f_max=20000, decay=0.1):
+        super().__init__()
 
-# Instantiate a video reader
-vc = cv.VideoCapture(str(vid_file))
+        self._f_min = torch.tensor(f_min, requires_grad=True)
+        self._f_max = torch.tensor(f_max, requires_grad=True)
+        self._decay = torch.tensor(decay, requires_grad=True)
 
-# Extract information about the video stream
-vid_w, vid_h, fps, tot_frames = get_video_meta(vc)
+        self._old_freq = None
 
-try:
-    # Instantiate an object tracker
-    tracker = MvTracker(vid_w, vid_h, max_dist=560, offset_x=50, canny_thres=args.canny_thres)
+    def __call__(self, theta):
+        """It is assumed that the input is a rotational speed in radians per second."""
+        
+        # Compute the current movement frequency
+        mv_freq = theta / (2 * np.pi)
 
-    # Track all objects
-    prog = tqdm(desc='Frames', total=tot_frames, unit='fps', position=0)
-    freqs: dict[int, list] = {}
-    pans: dict[int, dict[str, np.ndarray]] = {}
-    louds: dict[int, np.ndarray] = {}
+        # Apply decay if possible
+        if self._old_freq is not None:
+            mv_freq = self._old_freq * (1 - self._decay) + mv_freq * self._decay
+
+        # Store the movement frequency for later computation
+        self._old_freq = mv_freq
+
+        # Translate the movement frequency into a sound frequency in the interval [f_min, f_max]
+        return F.sigmoid(mv_freq) * (self._f_max - self._f_min) + self._f_min
+
+    def reset(self):
+        self._old_freq = None
+
+    def parameters(self):
+        return [self._f_min, self._f_max, self._decay]
+
+    def state_dict(self):
+        return dict(f_min=self._f_min, f_max=self._f_max, decay=self._decay)
+
+    def load_state_dict(self, params):
+        for k, v in params:
+            # Translate the attribute name to make it "private"
+            k = f'_{k}'
+            # Only set valid attributes, the rest is gracefully ignored
+            if hasattr(self, k):
+                setattr(self, k, v)
+
+
+if __name__ == "__main__":
+    # Define a command line interface
+    parser = ArgumentParser()
+    parser.add_argument('filename', type=str, help='Either the name of a video file to process, or a camera ID.')
+    parser.add_argument('-t', '--thres', dest='canny_thres', type=int, default=40)
+    parser.add_argument('-o', '--output', dest='out', type=str, required=True, help='Either the relative path to a file or the name of a device to which the audio will be written.')
+    parser.add_argument('-w', '--weight', dest='load', type=str, help="The relative path to the file from which to load the model's parameters.")
+    parser.add_argument('-l', '--learn_rate', dest='l_rate', type=float, help="The length of the step for each iteration of the gradient descent algorithm.", default=1e-3)
+    parser.add_argument('-r', '--sample_rate', dest='sample_rate', type=int, help="The sample rate to use for the audio output.", default=44100)
+
+    # And parse all command line arguments
+    args = parser.parse_args()
+
+    # Define where the audio output should go
+    if args.out in AudioStream.output_device_names:
+        audio_out = AudioStream(output_device_name=args.out, num_output_channels=1, sample_rate=args.sample_rate)
+    else:
+        fname = Path(args.out).expanduser().resolve()
+        audio_out = AudioFile(str(fname), samplerate=args.sample_rate, num_channels=1)
+
+    # Define where to take the video input from
+    try:
+        video_in = int(args.filename)
+    except ValueError:
+        video_in = str(Path(args.filename).expanduser().resolve())
+
+    # Instantiate model
+    model = Speed2SoundModel(f_min=100, f_max=300)
+    
+    # Load the parameters if necessary
+    if args.load is not None:
+        weight_file = Path(args.load).expanduser().resolve()
+        if weight_file.is_file():
+            state_dict = torch.load(weight_file, map_location='cpu')
+            model.load_state_dict(state_dict)
+        else:
+            logger.error(f'The provided weight file, does not exist or is not a file: {weight_file}')
+
+    # Declare the optimizer
+    # TODO: Modify the parameters to suit your needs
+    optimizer = torch.optim.SGD(model.parameters(), args.l_rate, maximize=False)
+
+    # TODO: Declare the loss
+    Loss = None
+
+    # Loop forever (or until the user presses ctrl+c and asks to quit the program)
     while True:
-        ret, frame = vc.read()
-        if not ret:
-            break
+        with VideoIterator(video_in) as vi:
+            # Get the video metadata
+            vid_w, vid_h, fps, tot_frames = get_video_meta(vi)
 
-        # Track detected objects
-        tracker.track(frame)
+            # Instantiate a new movement tracker
+            tracker = MvTracker(vid_w, vid_h, max_dist=560, offset_x=50, canny_thres=args.canny_thres)
+            
+            # Rest the gradient and the model
+            model.reset()
+            optimizer.zero_grad()
 
-        # Go through all the detected objects
-        for tid, traj in tracker.trajectories.items():
-            # Skip objects with a single point in their trajectory
-            if len(traj) < 2:
-                prev = np.zeros((2,), dtype=int)
-                curr = np.zeros((2,), dtype=int)
-                curr_polar = np.zeros((2,), dtype=int)
-                icrs = [(0, 0), (0, 0)]
-            else:
+            # Process the video and generate audio output
+            global_freqs = []
+            try:
+                for frame in vi:
+                    # Track objects
+                    tracker.track(frame)
 
-                # Skip objects with no instantaneous centers of rotations
-                if tid not in tracker.icrs:
-                    # Replace the instantaneous center of rotations with the detected object's own center
-                    icrs = [r.center for r in traj[-fps // 2:]]
-                else:
+                    # TODO: Can we assume that only a single object will be present?
+                    local_freqs = []
+                    chunks = []
+                    for obj in tracker.tracked_objects:
+                        if obj.id in tracker.rel_pos:
+                            # Compute the frequency
+                            old_pos, curr_pos = tracker.rel_pos[obj.id]
+                            theta = (old_pos[1] - curr_pos[1]) * fps
+                            freq = model(theta)
+                            local_freqs.append(freq)
 
-                    # Get the last few instantaneous centers of rotations
-                    icrs = tracker.icrs[tid][-fps // 2:]
+                            # Generate the associated audio chunk
+                            chunks.append(librosa.tone(freq, sr=args.sample_rate, length=int(args.sample_rate/fps)))
 
-                # Compute the coordinates of the icr
-                # We take the average of a few centers to filter out the jitter
-                icr = np.stack(icrs, axis=0).mean(axis=0).astype(int)
+                    # Store the list of local frequencies
+                    global_freqs.append(local_freqs)
 
-                # Translate the coordinates of the last and current position in the icr coordinate system
-                prev = cart_to_polar(*(np.array(traj[-2].center, dtype=int) - icr))
-                curr = cart_to_polar(*(np.array(traj[-1].center, dtype=int) - icr))
+                    # Mix the chunks together
+                    chunks = np.array(chunks).mean(axis=0).astype(np.float32)
 
-                # Convert the current position into polar coordinates for panning
-                curr_polar = cart_to_polar(*traj[-1].center)
+                    # Apply fade in/out to avoid clipping noises
+                    # TODO: If too aggressive, find a better way to fade
+                    amp = np.log(np.linspace(1, np.exp(1), num=int(args.sample_rate / (2 * fps))))
+                    chunks *= np.hstack([amp, amp[::-1]])
 
-            # Compute the angle between the two positions
-            theta = np.abs(curr[1] - prev[1])
+                    # Play the sound
+                    audio_out.write(chunks)
+                            
+            except KeyboardInterrupt:
+                pass
+            finally:
+                # Compute loss and optimize parameters
+                # TODO: provide the necessary targets to the loss function
+                loss = Loss(global_freqs, targ)
+                loss.backward()
+                optimizer.step()
 
-            # Compute the frequency of movement
-            freq = (theta * fps) / (2 * np.pi)
+                # Save parameters
+                state_dict = model.state_dict()
+                torch.save(state_dict, ROOT_DIR.joinpath('speed_2_sound.pt'))
 
-            # Filter frequency to smooth them out
-            if tid not in freqs:
-                freqs[tid] = [0] * tracker.starts[tid] + [freq]
-            else:
-                freqs[tid].append(freqs[tid][-1] * (1 - 0.05) + 0.05 * freq)
+                # Pause until an answer is given
+                res = input('Do you want to quit the program (y/N): ')
+                if res.lower() == 'y':
+                    break
 
-            # Compute the left, right panning
-            left = np.sqrt(2) / 2 * (np.sin(curr_polar[1]) + np.cos(curr_polar[1])) * np.ones((int(SAMPLE_RATE/fps), ))
-            right = np.sqrt(2) / 2 * (np.sin(curr_polar[1]) - np.cos(curr_polar[1])) * np.ones((int(SAMPLE_RATE/fps), ))
-
-            if tid not in pans:
-                pans[tid] = {'left': np.concatenate([np.full((int(SAMPLE_RATE * tracker.starts[tid]/fps), ), 0), left], axis=0), 
-                             'right': np.concatenate([np.full((int(SAMPLE_RATE * tracker.starts[tid]/fps), ), 0), right], axis=0)}
-            else:
-                pans[tid]['left'] = np.concatenate([pans[tid]['left'], left], axis=0)
-                pans[tid]['right'] = np.concatenate([pans[tid]['right'], right], axis=0)
-
-            # Compute the linear speed of the icr
-            #loud = np.full((int(SAMPLE_RATE / fps), ), min(500, freqs[tid][-1] * 2 * np.pi * curr[0]))
-
-            # Set loudness based on linear speed of icr
-            #if tid not in louds:
-            #    louds[tid] = np.concatenate([np.zeros((int(SAMPLE_RATE * tracker.starts[tid]/fps), )), loud], axis=0)
-            #else:
-            #    louds[tid] = np.concatenate([louds[tid], loud], axis=0)
-
-        # Move the progress bar forward
-        prog.update()
-
-except KeyboardInterrupt:
-    pass
-finally:
-    # Close the video stream
-    vc.release()
-
-    # Find extreme frequencies to normalize values to the [0, 1] interval
-    max_freq = 0
-    min_freq = np.inf
-    for fs in freqs.values():
-        max_fs = max(fs)
-        min_fs = min(fs)
-        if max_fs > max_freq:
-            max_freq = max_fs
-        if min_fs < min_freq:
-            min_freq = min_fs
-
-song = []
-for tid, fs in freqs.items():
-    # Clamp the various frequencies to the closest note
-    t_fs = librosa.note_to_hz(librosa.hz_to_note([220 + (440 * (f + 1e-4 - min_freq) / (max_freq - min_freq)) for f in fs])) # 1e-4 has been added to avoid division by zero when converting to note
-
-    # Generate the signal corresponding to the current tracked object
-    left, right = [], []
-    idx = 0
-    while idx < len(t_fs):
-        start = idx
-        idx += 1
-        while idx < len(t_fs) and t_fs[start] == t_fs[idx]:
-            idx += 1
-
-        # Generate the tone
-        tone = librosa.tone(t_fs[start], sr=SAMPLE_RATE, duration=(idx - start)/fps)
-
-        # Cross fade
-        nb_samples = int(SAMPLE_RATE / fps)
-        cross_amp = np.linspace(0, 1, num=nb_samples)
-        tone[-nb_samples:] *= cross_amp[::-1]  # Fade out the end
-        tone[0:nb_samples] *= cross_amp # Fade in the beginning
-
-        # Add the tone to both channels
-        left.append(tone)
-        right.append(tone)
-
-
-    # Compile the left and write channels
-    left = np.concatenate(left, axis=0)
-    right = np.concatenate(right, axis=0)
-
-    # Make sure they are of the right length (rounding errors in tone generation)
-    diff = pans[tid]['left'].shape[0] - left.shape[0]
-    if diff != 0:
-        left = np.concatenate([np.zeros((diff,)), left], axis=0)
-        right = np.concatenate([np.zeros((diff,)), right], axis=0)
-
-
-    # Apply the left/right panning
-    left *= pans[tid]['left']
-    right *= pans[tid]['right']
-
-    # Apply the loudness to both channels
-    #left *= louds[tid] / 100
-    #right *= louds[tid] / 100
-
-    # Append the signal to the others
-    song.append(np.stack([left, right], axis=1))
-
-# Mix all the signals together
-song = (np.stack(song, axis=0).mean(axis=0) * np.iinfo(np.int16).max).astype(np.int16)
-
-# Write the song to file
-wavfile.write(vid_file.with_suffix('.wav'), SAMPLE_RATE, song)
