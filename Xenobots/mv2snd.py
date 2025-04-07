19778199035238a7ceb10ac4af752faf07b41b4c
davinellulinvega <casparluc@gmail.com>
2025-04-07 15:55:45 +0900

    Replace chirps with shaped tones, and corrrectly open the audio output.

--- Xenobots/mv2snd.py
+++ Xenobots/mv2snd.py
@@ -26,6 +26,9 @@ class Speed2SoundModel(object):
 
     def __call__(self, theta):
         """It is assumed that the input is a rotational speed in radians per second."""
+
+        # Make sure the input is a tensor
+        theta = torch.tensor(theta)
         
         # Compute the current movement frequency
         mv_freq = theta / (2 * np.pi)
@@ -63,20 +66,39 @@ if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument('filename', type=str, help='Either the name of a video file to process, or a camera ID.')
     parser.add_argument('-t', '--thres', dest='canny_thres', type=int, default=40)
-    parser.add_argument('-o', '--output', dest='out', type=str, required=True, help='Either the relative path to a file or the name of a device to which the audio will be written.')
-    parser.add_argument('-w', '--weight', dest='load', type=str, help="The relative path to the file from which to load the model's parameters.")
-    parser.add_argument('-l', '--learn_rate', dest='l_rate', type=float, help="The length of the step for each iteration of the gradient descent algorithm.", default=1e-3)
-    parser.add_argument('-r', '--sample_rate', dest='sample_rate', type=int, help="The sample rate to use for the audio output.", default=44100)
+    parser.add_argument('-o', '--output', dest='out', type=str, default='default',
+                        help='Either the relative path to a file or the name of a device to which the audio will be written.')
+    parser.add_argument('-w', '--weight', dest='load', type=str,
+                        help="The relative path to the file from which to load the model's parameters.")
+    parser.add_argument('-l', '--learn_rate', dest='l_rate', type=float, default=1e-3,
+                        help="The length of the step for each iteration of the gradient descent algorithm.")
+    parser.add_argument('-v', '--volume', dest='vol', type=float, default=0.5,
+                        help="The volume in percentage at which to output audio. Defaults to: 50%")
+    parser.add_argument('-r', '--sample_rate', dest='sample_rate', type=int, default=44100,
+                        help="The sample rate to use for the audio output.")
+    parser.add_argument('--list', dest='lst_devices', action='store_true',
+                        help='List all available output audio devices.')
 
     # And parse all command line arguments
     args = parser.parse_args()
 
+    # If requested, list the output audio devices
+    if args.lst_devices:
+        logger.info('The following output devices are available:')
+        for dev_name in AudioStream.output_device_names:
+            logger.info(dev_name)
+
+        # And exit
+        exit(0)
+
     # Define where the audio output should go
     if args.out in AudioStream.output_device_names:
-        audio_out = AudioStream(output_device_name=args.out, num_output_channels=1, sample_rate=args.sample_rate)
+        audio_out_cls = AudioStream
+        audio_out_kwargs = dict(output_device_name=args.out, num_output_channels=1, sample_rate=args.sample_rate) 
     else:
         fname = Path(args.out).expanduser().resolve()
-        audio_out = AudioFile(str(fname), samplerate=args.sample_rate, num_channels=1)
+        audio_out_cls = AudioFile
+        audio_out_kwargs = dict(filename=str(fname), samplerate=args.sample_rate, num_channels=1)
 
     # Define where to take the video input from
     try:
@@ -105,63 +127,69 @@ if __name__ == "__main__":
 
     # Loop forever (or until the user presses ctrl+c and asks to quit the program)
     while True:
-        with VideoIterator(video_in) as vi:
-            # Get the video metadata
-            vid_w, vid_h, fps, tot_frames = get_video_meta(vi)
+        with audio_out_cls(**audio_out_kwargs) as audio_out:
+            with VideoIterator(video_in) as vi:
+                # Get the video metadata
+                vid_w, vid_h, fps, tot_frames = get_video_meta(vi)
 
-            # Instantiate a new movement tracker
-            tracker = MvTracker(vid_w, vid_h, max_dist=560, offset_x=50, canny_thres=args.canny_thres)
-            
-            # Rest the gradient and the model
-            model.reset()
-            optimizer.zero_grad()
+                # Instantiate a new movement tracker
+                tracker = MvTracker(vid_w, vid_h, max_dist=560, offset_x=50, canny_thres=args.canny_thres)
+                
+                # Rest the gradient and the model
+                model.reset()
+                optimizer.zero_grad()
 
-            # Process the video and generate audio output
-            freqs = []
-            try:
-                for frame in vi:
-                    # Track objects
-                    tracker.track(frame)
+                # Pre-compute the amplitude to apply to all audio chunks
+                # This is to prevent any clipping noise between chunks
+                fade_len = int(args.sample_rate/(10*fps))
+                full_len = int(args.sample_rate/fps) - 2 * fade_len
+                amp = np.log(np.linspace(1, np.exp(1), fade_len))
+                amp = np.hstack([amp, np.ones((full_len, )), amp[::-1]]).astype(np.float32) * args.vol
 
-                    # We assume that only a single object is tracked
-                    # TODO: If assumption is not correct, then manage a list of models, and save their parameters accordingly
-                    try:
-                        obj = tracker.tracked_objects[0]
-                    except IndexError:
-                        continue
+                # Process the video and generate audio output
+                freqs = []
+                try:
+                    for frame in vi:
+                        # Track objects
+                        tracker.track(frame)
 
-                    if obj.id in tracker.rel_pos:
-                        # Compute the frequency
-                        old_pos, curr_pos = tracker.rel_pos[obj.id]
-                        theta = (curr_pos[1] - old_pos[1]) * fps
-                        freq = model(theta)
+                        # We assume that only a single object is tracked
+                        # TODO: If assumption is not correct, then manage a list of models, and save their parameters accordingly
+                        try:
+                            obj = tracker.tracked_objects[0]
+                        except IndexError:
+                            continue
 
-                        # Generate the associated audio chunk
-                        prev_freq = freqs[-1].detach().cpu().item()
-                        curr_freq = freq.detach().cpu().item()
-                        chunk = librosa.chirp(fmin=prev_freq, fmax=curr_freq, sr=args.sample_rate, length=int(args.sample_rate/fps), linear=False).astype(np.float32)
+                        if obj.id in tracker.rel_pos:
+                            # Compute the frequency
+                            old_pos, curr_pos = tracker.rel_pos[obj.id]
+                            theta = (curr_pos[1] - old_pos[1]) * fps
+                            freq = model(theta)
 
-                        # Store the list of local frequencies
-                        freqs.append(freq)
+                            # Generate the associated audio chunk
+                            chunk = librosa.tone(frequency=freq.detach().cpu().item(), sr=args.sample_rate, length=int(args.sample_rate/fps)).astype(np.float32)
 
-                        # Play the sound
-                        audio_out.write(chunk)
-                            
-            except KeyboardInterrupt:
-                pass
-            finally:
-                # Compute loss and optimize parameters
-                # TODO: provide the necessary targets to the loss function
-                loss = Loss(freqs, targ)
-                loss.backward()
-                optimizer.step()
+                            # Store the list of local frequencies
+                            freqs.append(freq)
 
-                # Save parameters
-                state_dict = model.state_dict()
-                torch.save(state_dict, ROOT_DIR.joinpath('speed_2_sound.pt'))
+                            # Play the sound
+                            audio_out.write(chunk * amp, sample_rate=args.sample_rate)
+                                
+                except KeyboardInterrupt:
+                    pass
+                finally:
+                    # Compute loss and optimize parameters
+                    # TODO: provide the necessary targets to the loss function
+                    loss = Loss(freqs, targ)
+                    loss.backward()
+                    optimizer.step()
 
-                # Pause until an answer is given
-                res = input('Do you want to quit the program (y/N): ')
-                if res.lower() == 'y':
-                    break
+                    # Save parameters
+                    state_dict = model.state_dict()
+                    torch.save(state_dict, ROOT_DIR.joinpath('speed_2_sound.pt'))
+
+                    # Pause until an answer is given
+                    res = input('Do you want to quit the program (y/N): ')
+                    if res.lower() == 'y':
+                        break
 
