cd85853797defea64cb0b38050e0130d5b36da13
luc-caspar <luc.caspar@cross-compass.com>
2025-08-13 19:29:46 +0900

    A simple script to test the movement tracker.

--- Xenobots/track_mv.py
+++ Xenobots/track_mv.py
@@ -2,55 +2,40 @@
 import cv2 as cv
 from tqdm import tqdm
 
-from utils import MvTracker, get_video_meta, VideoIterator
+from utils import MvTracker, VideoIterator
 
 
-colors = [
-    (40, 42, 54),
-    (248, 248, 242),
-    (139, 233, 253),
-    (80, 250, 123),
-    (255, 184, 108),
-    (255, 121, 198),
-    (68, 71, 90),
-    (189, 147, 249),
-    (255, 85, 85),
-    (241, 250, 140),
-    (98, 114, 164)
-]
+DEBUG = True
 # Set the threshold for canny edge detection
 thres = 40
 
-win_name = 'Debug'
-
 # Declare a video input
 with VideoIterator('Data/test.mov') as vi:
-    # Create a named window to display the results
-    cv.namedWindow(win_name, cv.WINDOW_NORMAL | cv.WINDOW_KEEPRATIO | cv.WINDOW_GUI_NORMAL)
 
     # Extract information about the video
-    vid_w, vid_h, fps, tot_frames = get_video_meta(vi)
+    vid_w, vid_h, fps, tot_frames = vi.get_metadata()
 
-    # Instantiate a movement tracker
-    tracker = MvTracker(vid_w, vid_h, max_dist=560, offset_x=50)
-    prog = tqdm(desc='Frames', total=tot_frames, unit='fps', position=0)
-    for frame in vi:
-        # Track objects
-        tracker.track(frame)
-        for obj in tracker.tracked_objects:
-            center = obj.estimate.squeeze(axis=0).astype(int).tolist()
-            cv.circle(frame, center, 2, colors[obj.id%len(colors)][::-1], -1)
+    if DEBUG:
+        # Initialize video writer used for debugging
+        vw = cv.VideoWriter(str('Data/dbg.mp4'), cv.VideoWriter_fourcc(*'mp4v'), fps, (vid_w, vid_h), True)  # True indicate that the video is in color
 
-            # Draw Instantaneous Center of Rotation
-            try:
-                icr = tracker.icrs[obj.id].astype(int).tolist()
-                frame = cv.circle(frame, icr, 1, (0, 0, 255), 3)
-            except KeyError:
-                pass
+    try:
+        # Instantiate a movement tracker
+        tracker = MvTracker(vid_w, vid_h, debug=DEBUG)
+        prog = tqdm(desc='Frames', total=tot_frames, unit='fps', position=0)
+        for frame in vi:
+            # Track objects
+            tracker.track(frame)
 
-        cv.imshow(win_name, frame)
-        cv.pollKey()
-        prog.update()
+            if DEBUG:
+                # Write debug video
+                vw.write(frame)
 
-# Close all windows
-cv.destroyAllWindows()
+            # Increase progress bar
+            prog.update()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        if DEBUG:
+            # Close video file
+            vw.release()
